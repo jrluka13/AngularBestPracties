4ee082d53bf105c8241586293d89e24119d79b8a
Vlad Lukashevich <v.lukashevich@itransition.com>
2021-06-17 13:01:11 +0300

    added modules for pages

--- src/layouts/admin/admin-layout.module.ts
+++ src/layouts/admin/admin-layout.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { AdminLayoutComponent } from './admin-layout.component';
-import { RolesComponent } from './pages/roles/roles.component';
-import { UsersComponent } from './pages/users/users.component';
+import { AdminRoutingModule } from './admin-routing.module';
+import { CommonModule } from '@angular/common';
+import { RolesPageModule } from './pages/roles/roles-page.module';
+import { UsersPageModule } from './pages/users/users-page.module';
 
 @NgModule({
-  declarations: [AdminLayoutComponent, RolesComponent, UsersComponent],
-  imports: [],
+  declarations: [AdminLayoutComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    RolesPageModule,
+    UsersPageModule,
+  ],
   providers: [],
 })
 
