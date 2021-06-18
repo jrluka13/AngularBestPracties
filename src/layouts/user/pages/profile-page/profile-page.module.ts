f7a1ccc9cce5f8845d86c6184cc6f678faebcec1
Vlad Lukashevich <v.lukashevich@itransition.com>
2021-06-18 12:15:32 +0300

    add grid profile page

--- src/layouts/user/pages/profile-page/profile-page.module.ts
+++ src/layouts/user/pages/profile-page/profile-page.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfilePageComponent } from './profile-page.component';
+import { SharedModule } from '../../../../shared/shared.module';
 
 @NgModule({
   declarations: [ProfilePageComponent],
   exports: [ProfilePageComponent],
   imports: [
-    CommonModule
+    CommonModule, SharedModule
   ]
 })
 
-export class ProfilePageModule{}
+export class ProfilePageModule {
+}
