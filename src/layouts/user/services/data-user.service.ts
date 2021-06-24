f6318b4d2a5289edf2b7ac4e68303f36ac609494
Vlad Lukashevich <v.lukashevich@itransition.com>
2021-06-24 12:23:50 +0300

    added CRUD for posts

--- src/layouts/user/services/data-user.service.ts
+++ src/layouts/user/services/data-user.service.ts
@@ -7,13 +7,24 @@ export class DataUserService {
   }
 
   getData(): void {
-    this.http.get('https://jsonplaceholder.typicode.com/todos?_limit=2')
+    this.http.get('https://jsonplaceholder.typicode.com/posts?_limit=2')
       .subscribe((response) => {
         console.log(response);
       });
   }
 
-  deleteData(): void {
+  deleteData(id: number): void {
+    this.http.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  }
 
+  postData(newPost: object): void {
+    this.http.post('https://jsonplaceholder.typicode.com/posts', newPost)
+      .subscribe((post) => {
+        console.log('post', post);
+      });
+  }
+
+  updateData(id: number, updatedPost: object): void {
+    this.http.put(`https://jsonplaceholder.typicode.com/posts/${id}`, updatedPost);
   }
 }
